bf3f870b3bb12e6ff5104f5b50a8d61c262e6154
Nikhil Jindal <nxj224@case.edu>
2024-04-10 14:35:46 +0000

    default output with 1 thread

--- loui_code/mutex.c
+++ loui_code/mutex.c
@@ -9,7 +9,7 @@ int count = 0, currval;
 pthread_mutex_t myptmlock = PTHREAD_MUTEX_INITIALIZER;
 int inc(void) {
 	if (pthread_mutex_lock(&myptmlock)) printf("ERROR\n");
-	currval = count; currval++; sleep(.2); count = currval;
+	currval = count; currval++; sleep(.1); count = currval;
 	pthread_mutex_unlock(&myptmlock);
 }
 
@@ -30,7 +30,7 @@ void *myThreadFun(void *vargp) {
 }
 
 int main() {
-	pthread_t thread_id[2]; char command[80];
+	pthread_t thread_id[1]; char command[80];
 	cpu_set_t mask; CPU_ZERO(&mask); CPU_SET(0, &mask); sched_setaffinity(0, sizeof(mask), &mask);
 	for (int i=0; i<2; i++) pthread_create(&(thread_id[i]), NULL, myThreadFun, NULL);
 	for (int i=0; i<2; i++) pthread_join(thread_id[i], NULL);
